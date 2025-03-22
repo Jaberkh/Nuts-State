86fc5c93c3c095f46d1d2b2a15b082fd74229e33
basednuts <basenuts7@gmail.com>
2025-03-22 12:35:28 +0330

    update1

--- src/index.tsx
+++ src/index.tsx
@@ -425,6 +425,7 @@ async function getUserDataFromCache(fid: string): Promise<{
   if (ogNFTCount > 0 || newNFTCount > 0) {
     remainingAllowance = `${maxAllowance} / ${Math.max(maxAllowance - sentPeanutCount, 0)}`;
     reduceEndSeason = sentPeanutCount > maxAllowance ? String(sentPeanutCount - maxAllowance) : '';
+    OGpic=1;
   } else {
     if (ALLOW_NON_HOLDERS) {
       remainingAllowance = `${maxAllowance} / ${Math.max(maxAllowance - sentPeanutCount, 0)}`;
